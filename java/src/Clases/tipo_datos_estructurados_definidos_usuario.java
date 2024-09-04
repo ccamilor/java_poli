c7ce777cc8c3767d1f63e4d2971f07099260519d
ccamilor <cris.camilo.9504@gmail.com>
2024-09-04 17:55:30 -0500

    Tipos de datos estructurados definidos por el usuario

---
+++ java/src/Clases/tipo_datos_estructurados_definidos_usuario.java
@@ -0,0 +1,21 @@
+package Clases;
+
+import java.io.BufferedReader;
+
+public class tipo_datos_estructurados_definidos_usuario {
+
+    //Se emplean para efectuar casi cualquier operación o tarea:
+    //leer y escribir archivos, enviar correos electrónicos, cadenas especializadas etc.
+
+    //Variable de tipo persona (Persona es una clase);
+    Persona P;
+    //Variable de tipo animal (Animal es una clase);
+    Animal A;
+    //Variable de Java de la clase Math.
+    Math M;
+    //Variable de Java de la clase Scanner.
+    Scanner S;
+    //Variable de Java de la clase BufferedReader.
+    BufferedReader B;
+
+}
