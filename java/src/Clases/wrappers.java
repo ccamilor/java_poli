59779407d6b31f6552801d0caa114b694257b766
ccamilor <cris.camilo.9504@gmail.com>
2024-09-05 17:15:35 -0500

    Wrappers y Casteo de tipo de Variables

---
+++ java/src/Clases/wrappers.java
@@ -0,0 +1,28 @@
+package Clases;
+
+public class wrappers {
+    //Wrappers
+
+
+    //Estos tipos son equivalentes a los primitivos pero en forma de
+    //objetos. Son: Byte, Short, Integer, Long, Float, Double,
+    //Boolean y Character (8 igualmente)
+
+    //Representación de byte en Byte
+    Byte numeroByte = 1;
+    //Representación de shor en Short
+    Short numeroShort = 2416;
+    //Representación de int en Integer.
+    int numeroInteger = 95256712;
+    //Representación de long en Long
+    Long numeroLong = 5213714121L;
+    //Representación de float en Float
+    Float numeroFloat = 6591342543251F;
+    //Representación de double en Double
+    Double numeroDouble = 9.3;
+    //Representación de boolean en Boolean
+    Boolean numeroBoolean = true;
+    //Representación de char en Character
+    Character variableCharacter = 'A';
+    Character getVariableCharacter = 2;
+}
